bb4e04007d7a2d5a9c3972a0370f78e472e14c9c
GitHub Action <action@github.com>
2024-07-07 02:36:44 +0800

    Auto Update 2024-07-07 02:36

--- Loon/Script/lodepuly/GaoDing/GaoDing_remove_ads.js
+++ Loon/Script/lodepuly/GaoDing/GaoDing_remove_ads.js
@@ -1,25 +1,23 @@
-// 2024-07-06 20:44:51
-if ($request.url.includes('/exhibitions/template/resources')) {
-    var json = JSON.parse($response.body);
+// 2024-07-06 23:06:39
 
-    var indicesToDelete = [0, 1];
+var json = JSON.parse($response.body);
 
-    indicesToDelete.forEach(function (index) {
-        if (json.pits[index]) {
-            for (var prop in json.pits[index]) {
-                delete json.pits[index][prop];
-            }
-        }
-    });
-
-    $done({ body: JSON.stringify(json) });
-} else if ($request.url.includes('/exhibitions/app_mine/resources')) {
-    var json = JSON.parse($response.body);
-
-    if (json.finance_tab) {
-        delete json.finance_tab;
+if ($request.url.includes("/oc/exhibitions/template/resources")) {
+    // 移除首页轮播图
+    if (json.pits && json.pits[0]) {
+        delete json.pits[0].delivery_materials;
     }
-    $done({ body: JSON.stringify(json) });
-} else {
-    $done({});
+    // // 移除首页会员入口
+    // if (json.pits && json.pits[1]) {
+    //     delete json.pits[1].materials;
+    // }
 }
+
+if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
+    // 移除我的页面轮播图
+    if (json.pits && json.pits[2]) {
+        delete json.pits[2].delivery_materials;
+    }
+}
+
+$done({ body: JSON.stringify(json) });
