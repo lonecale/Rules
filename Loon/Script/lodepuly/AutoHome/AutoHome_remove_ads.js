f895bcd70c325b37657387a266c7ea9c1438fb08
GitHub Action <action@github.com>
2024-07-17 14:40:52 +0800

    Auto Update 2024-07-17 14:40

--- Loon/Script/lodepuly/AutoHome/AutoHome_remove_ads.js
+++ Loon/Script/lodepuly/AutoHome/AutoHome_remove_ads.js
@@ -1,4 +1,4 @@
-// 2024-07-17 09:16:53
+// 2024-07-17 11:27:46
 let url = $request.url;
 try {
     let obj = JSON.parse($response.body);
@@ -19,28 +19,35 @@ try {
     }
 
     // 删除选车 - 新车报价页面直播内容
-    function removeItemsWithKeywords(data, keywords) {
-        if (Array.isArray(data)) {
-            data = data.filter(item => {
-                return !item.text || !keywords.some(keyword => item.text.includes(keyword));
-            });
-        } else if (typeof data === 'object') {
-            for (let key in data) {
-                if (data[key] && typeof data[key] === 'object') {
-                    data[key] = removeItemsWithKeywords(data[key], keywords);
+    function removeItemsWithKeywords(objc, keyword) {
+        var otherlist = objc["result"]["otherlist"]
+        if (Array.isArray(otherlist)) {
+          otherlist = otherlist.filter(function(ol) {
+            var lists = ol["list"]
+            if (Array.isArray(lists)) {
+              for (let index in lists) {
+                var item = lists[index]
+                if (item["title"] === keyword) {
+                  return false
                 }
+              }
             }
+            return true
+          });
         }
-        return data;
+        objc["result"]["otherlist"] = otherlist
+        return objc
     }
 
     if (url.includes("/carstreaming/selectcarportal/seriestopwithtagscard")) {
-        obj = removeItemsWithKeywords(obj, ["直播中", "报价中"]);
+        obj = removeItemsWithKeywords(obj, "直播中");
+        obj = removeItemsWithKeywords(obj, "报价中");
     }
 
     // 删除二手车 - 竖版轮播图
-    if (/\/apic\/v\d+\/gethomepagefeed\//.test(url)) {
-        obj = removeItemsWithKeywords(obj, ["车抵贷"]);
+    if (/\/apic\/v\d+\/gethomepagefeed/.test(url)) {
+        // console.log("删除车抵贷");
+        obj = removeItemsWithKeywords(obj, "车抵贷");
     }
 
     // 删除我的页面 - 移除添加我的爱车领券
@@ -50,9 +57,12 @@ try {
 
     // 遍历关键词删除所属对象
     if (/\/platform\/carserver\/((usercenter\/getservicecards)|(carcard\/(mycardv\d+|allcard)))/.test(url)) {
-        obj = removeItemsWithKeywords(obj, ["低息借钱", "分期购车", "车主贷"]);
+        obj = removeItemsWithKeywords(obj, "低息借钱");
+        obj = removeItemsWithKeywords(obj, "分期购车");
+        obj = removeItemsWithKeywords(obj, "车主贷");
     }
 
+    console.log(JSON.stringify(obj))
     $done({ body: JSON.stringify(obj) });
 } catch (e) {
     console.error(e);
