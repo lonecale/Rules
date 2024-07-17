cb3c0355a99a1069cc24fdcc5393410886e25938
GitHub Action <action@github.com>
2024-07-18 02:39:50 +0800

    Auto Update 2024-07-18 02:39

--- Loon/Script/lodepuly/Kuwo/Kuwo_remove_ads.js
+++ Loon/Script/lodepuly/Kuwo/Kuwo_remove_ads.js
@@ -1,4 +1,4 @@
-// 2024-07-17 20:09:28
+// 2024-07-18 02:27:39
 const url = $request.url;
 const body = $response.body;
 
@@ -6,25 +6,25 @@ if (!body) $done({});
 
 let obj = JSON.parse(body);
 
-const pathRegex = /\/ocean\/v\d\/tab\/list_v\d/;
-const namesToRemove = ["AI帮唱", "长相思2", "K歌", "小说", "游戏"];
+const pathRegex = /\/openapi\/v\d\/tingshu\/index\/radio/;
+const typesToRemove = ["banner", "stripAdvert"]; // 删除听书页面轮播图和横幅
 
 if (pathRegex.test(url)) {
-    function removeParentIfNameMatches(obj) {
+    function removeParentIfTypeMatches(obj) {
         if (typeof obj === 'object' && obj !== null) {
             for (let key in obj) {
                 if (obj[key] && typeof obj[key] === 'object') {
-                    if (namesToRemove.includes(obj[key].name)) {
+                    if (typesToRemove.includes(obj[key].type)) {
                         delete obj[key];
                     } else {
-                        removeParentIfNameMatches(obj[key]);
+                        removeParentIfTypeMatches(obj[key]);
                     }
                 }
             }
         }
     }
 
-    removeParentIfNameMatches(obj);
+    removeParentIfTypeMatches(obj);
 }
 
 $done({ body: JSON.stringify(obj) });
