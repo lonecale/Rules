782f9b860a785cd51f7ab043208a2fa9165eafe4
GitHub Action <action@github.com>
2024-08-13 20:54:21 +0800

    Auto Update 2024-08-13 20:54

--- Loon/Script/lodepuly/CatEarFM/CatEarFM_remove_ads.js
+++ Loon/Script/lodepuly/CatEarFM/CatEarFM_remove_ads.js
@@ -1,4 +1,4 @@
-// 2024-08-12 22:41:30
+// 2024-08-13 19:45:41
 const url = $request.url;
 if (url.includes("/site/icons")) {
     let obj = JSON.parse($response.body);
@@ -6,6 +6,14 @@ if (url.includes("/site/icons")) {
         obj.info.icons = obj.info.icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
     }
     $done({ body: JSON.stringify(obj) });
+} else if (url.includes("/discovery/list")) {
+    let obj = JSON.parse($response.body);
+    for (const index in obj.info) {
+        obj.info[index] = obj.info[index].filter(item => {
+          return !(item.title === '直播间' || item.title === '广播剧' || item.title === '免流服务')
+        })
+    }
+    $done({body: JSON.stringify(obj)})
 } else {
     $done({});
 }
